73e0a9b00a5a5554bfd8d6009154dad31f4bbdcb
Ferzin Zeynal <ferzo.zeynal@mail.ru>
2022-12-13 00:18:27 +0400

    login page implemented

---
+++ app/src/main/java/az/avtomatika/autosoft/MainAppication.kt
@@ -0,0 +1,12 @@
+package az.avtomatika.autosoft
+
+import android.app.Application
+
+class MainAppication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
--- app/src/main/java/az/avtomatika/autosoft/ui/MainActivity.kt
+++ app/src/main/java/az/avtomatika/autosoft/ui/MainActivity.kt
@@ -1,11 +1,16 @@
-package az.avtomatika.autosoft
+package az.avtomatika.autosoft.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import az.avtomatika.autosoft.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
